6c9ffbfb66cd43f697708a49b2c9a84e4a7ed01c
anxhela <anxhelaristani19@gmail.com>
2020-10-21 22:13:27 +0200

    db settings

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
  
 if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
     $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
-    define(‘DB_NAME’, trim($db[`path`],`/`));
-    define(‘DB_USER’, $db[`user`]);
-    define(‘DB_PASSWORD’, $db[`pass`]);
-    define(‘DB_HOST’, $db[`host`]);
+    define(‘DB_NAME’, 'bennydb'));
+    define(‘DB_USER’, 'benny');
+    define(‘DB_PASSWORD’, 'PPEmRlgDXiDbrugM');
+    define(‘DB_HOST’, 'localhost');
     define(‘DB_CHARSET’, ‘utf8’);
     define(‘DB_COLLATE’, ‘’);
 } else {
