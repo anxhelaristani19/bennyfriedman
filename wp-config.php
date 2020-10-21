0f0aaeee6132bac0ae6e55dc1efc689a4b9af379
Joana Karanxha <joana@paperclicks.net>
2020-10-22 00:17:14 +0200

    fix

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,7 @@
  
 if(getenv('CLEARDB_DATABASE_URL')) {
     $db = parse_url('mysql://bb8daa6b60c69a:374c5b0f@us-cdbr-east-02.cleardb.com/heroku_b019d8867a9174f?reconnect=true');
+    define(‘DB_NAME’,'benny db');
 } else {
     die;
 }
