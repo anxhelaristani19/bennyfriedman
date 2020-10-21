cab91fa2d2c612c2e6f0bc35ac396b526d243a28
Joana Karanxha <joana@paperclicks.net>
2020-10-22 00:31:04 +0200

    fix

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,12 @@
  
 if(getenv('CLEARDB_DATABASE_URL')) {
     $db = parse_url('mysql://bb8daa6b60c69a:374c5b0f@us-cdbr-east-02.cleardb.com/heroku_b019d8867a9174f?reconnect=true');
-    define(‘DB_NAME’,'benny db');
+    define(‘DB_NAME’,`heroku_b019d8867a9174f`);
+    define(‘DB_USER’, `bb8daa6b60c69a`);
+    define(‘DB_PASSWORD’, `374c5b0f`);
+    define(‘DB_HOST’, `us-cdbr-east-02.cleardb.com`);
+    define(‘DB_CHARSET’, ‘utf8’);
+    define(‘DB_COLLATE’, ‘’);
 } else {
     die;
 }
